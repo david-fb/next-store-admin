9a3f20ba861825a5b70972bfb9f080129032a438
David Basto <davidbasto01@gmail.com>
2022-03-23 12:25:06 -0500

    commented code deleted

--- src/pages/dashboard/index.js
+++ src/pages/dashboard/index.js
@@ -3,7 +3,7 @@ import useFetch from '@hooks/useFetch';
 import { ChevronLeftIcon, ChevronRightIcon } from '@heroicons/react/solid';
 import { useState, useEffect } from 'react';
 
-const PRODUCT_LIMIT = 1;
+const PRODUCT_LIMIT = 5;
 
 export default function Dashboard() {
   const [productOffset, setProductOffset] = useState(0);
@@ -36,6 +36,9 @@ export default function Dashboard() {
   }, [currentPage]);
 
   //Pagination buttons
+  {
+    /* Current: "z-10 bg-indigo-50 border-indigo-500 text-indigo-600", Default: "bg-white border-gray-300 text-gray-500 hover:bg-gray-50" */
+  }
   const paginationButtons = [];
   //Comprueba si se puede hacer los 5 botones
   if (pages.length > 5) {
@@ -233,38 +236,7 @@ export default function Dashboard() {
                     <span className="sr-only">Previous</span>
                     <ChevronLeftIcon className="h-5 w-5" aria-hidden="true" />
                   </button>
-                  {/* Current: "z-10 bg-indigo-50 border-indigo-500 text-indigo-600", Default: "bg-white border-gray-300 text-gray-500 hover:bg-gray-50" */}
-                  {/* {pages.map((page, index) => (
-                  <button
-                    onClick={() => handlePageClick(index)}
-                    key={`Button-page-${index}`}
-                    className={`${
-                      index === currentPage ? 'z-10 bg-indigo-50 border-indigo-500 text-indigo-600' : 'bg-white border-gray-300 text-gray-500 hover:bg-gray-50'
-                    } relative inline-flex items-center px-4 py-2 border text-sm font-medium`}
-                  >
-                    {index + 1}
-                  </button>
-                ))} */}
                   {paginationButtons}
-                  {/* <a href="/" aria-current="page" className="z-10 bg-indigo-50 border-indigo-500 text-indigo-600 relative inline-flex items-center px-4 py-2 border text-sm font-medium">
-                  1
-                </a>
-                <a href="/" className="bg-white border-gray-300 text-gray-500 hover:bg-gray-50 relative inline-flex items-center px-4 py-2 border text-sm font-medium">
-                  2
-                </a>
-                <a href="/" className="bg-white border-gray-300 text-gray-500 hover:bg-gray-50 hidden md:inline-flex relative items-center px-4 py-2 border text-sm font-medium">
-                  3
-                </a>
-                <span className="relative inline-flex items-center px-4 py-2 border border-gray-300 bg-white text-sm font-medium text-gray-700">...</span>
-                <a href="/" className="bg-white border-gray-300 text-gray-500 hover:bg-gray-50 hidden md:inline-flex relative items-center px-4 py-2 border text-sm font-medium">
-                  8
-                </a>
-                <a href="/" className="bg-white border-gray-300 text-gray-500 hover:bg-gray-50 relative inline-flex items-center px-4 py-2 border text-sm font-medium">
-                  9
-                </a>
-                <a href="/" className="bg-white border-gray-300 text-gray-500 hover:bg-gray-50 relative inline-flex items-center px-4 py-2 border text-sm font-medium">
-                  10
-                </a> */}
                   <button
                     onClick={handleNextPage}
                     className="relative inline-flex items-center px-2 py-2 rounded-r-md border border-gray-300 bg-white text-sm font-medium text-gray-500 hover:bg-gray-50"
